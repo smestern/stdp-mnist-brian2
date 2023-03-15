36e6f7e8545448fb86d68bceaa5a9a46a7dd3122
smestern <smestern@uwo.ca>
2023-03-15 08:33:05 -0400

    recompile so connections are not rebuild every run

--- spiking_RT_refactor.py
+++ spiking_RT_refactor.py
@@ -117,169 +117,6 @@ def normalize_weights():
         temp_conn[:,j] *= colFactors[j]
     connections['XeAe'].w = temp_conn[connections['XeAe'].i, connections['XeAe'].j]
 
-def run_network(j, rate, test_mode = False):
-    device.reinit()
-    device.reactivate()
-    
-    
-
-    single_example_time =   0.35 * b.second
-    resting_time = 0.15 * b.second
-    runtime = num_examples * (single_example_time + resting_time)
-
-    use_testing_set       = True # add
-
-    v_rest_e = -65. * b.mV
-    v_rest_i = -60. * b.mV
-    v_reset_e = -65. * b.mV
-    v_reset_i = 'v=-45.*mV'
-    v_thresh_i = 'v>-40.*mV'
-    refrac_e = 5. * b.ms
-    refrac_i = 2. * b.ms
-
-
-    tc_pre_ee = 20*b.ms
-    tc_post_1_ee = 20*b.ms
-    tc_post_2_ee = 40*b.ms
-    nu_ee_pre =  0.0001      # learning rate
-    nu_ee_post = 0.01       # learning rate
-    wmax_ee = 1.0
-
-    if test_mode:
-        scr_e = 'v = v_reset_e; timer = 0*ms'
-    else:
-        tc_theta = 1e7 * b.ms
-        theta_plus_e = 0.05 * b.mV
-        scr_e = 'v = v_reset_e; theta += theta_plus_e; timer = 0*ms'
-    offset = 20.0*b.mV
-    v_thresh_e = '(v>(theta - offset + -52.*mV)) and (timer>refrac_e)'
-
-
-    neuron_eqs_e = '''
-            dv/dt = ((v_rest_e - v) + (I_synE+I_synI) / nS) / (100*ms)  : volt (unless refractory)
-            I_synE = ge * nS *         -v                           : amp
-            I_synI = gi * nS * (-100.*mV-v)                          : amp
-            dge/dt = -ge/(1.0*ms)                                   : 1
-            dgi/dt = -gi/(2.0*ms)                                  : 1
-            '''
-    if test_mode:
-        neuron_eqs_e += '\n  theta      :volt'
-    else:
-        neuron_eqs_e += '\n  dtheta/dt = -theta / (tc_theta)  : volt'
-    neuron_eqs_e += '\n  dtimer/dt = 0.1  : second'
-
-    neuron_eqs_i = '''
-            dv/dt = ((v_rest_i - v) + (I_synE+I_synI) / nS) / (10*ms)  : volt (unless refractory)
-            I_synE = ge * nS *         -v                           : amp
-            I_synI = gi * nS * (-85.*mV-v)                          : amp
-            dge/dt = -ge/(1.0*ms)                                   : 1
-            dgi/dt = -gi/(2.0*ms)                                  : 1
-            '''
-
-    eqs_stdp_ee = '''
-                    post2before                            : 1
-                    dpre/dt   =   -pre/(tc_pre_ee)         : 1 (event-driven)
-                    dpost1/dt  = -post1/(tc_post_1_ee)     : 1 (event-driven)
-                    dpost2/dt  = -post2/(tc_post_2_ee)     : 1 (event-driven)
-                '''
-    eqs_stdp_pre_ee = 'pre = 1.; w = clip(w - nu_ee_pre * post1, 0, wmax_ee)'
-    eqs_stdp_post_ee = 'post2before = post2; w = clip(w + nu_ee_post * pre * post2before, 0, wmax_ee); post1 = 1.; post2 = 1.'
-
-    neuron_groups = {}
-    input_groups = {}
-    connections = {}
-    spike_counters = {}
-    
-
-    neuron_groups['Ae'] = b.NeuronGroup(n_e, neuron_eqs_e, threshold= v_thresh_e, refractory= refrac_e, reset= scr_e, method='euler')
-    neuron_groups['Ai'] = b.NeuronGroup(n_i, neuron_eqs_i, threshold= v_thresh_i, refractory= refrac_i, reset= v_reset_i, method='euler')
-
-
-    #------------------------------------------------------------------------------
-    # create network population and recurrent connections
-    #------------------------------------------------------------------------------
-    print( 'create neuron group A')
-
-    neuron_groups['Ae'].v = v_rest_e - 40. * b.mV
-    neuron_groups['Ai'].v = v_rest_i - 40. * b.mV
-    if test_mode:
-        neuron_groups['Ae'].theta = np.load(data_path + 'weights/theta_A.npy') * b.volt
-    else:
-        neuron_groups['Ae'].theta = np.ones((n_e)) * 20.0*b.mV
-
-    print( 'create recurrent connections')
-    weightMatrix = get_matrix_from_file(data_path + 'random/AeAi.npy', n_e, n_i)
-    connections['AeAi'] = b.Synapses(neuron_groups['Ae'], neuron_groups['Ai'], model='w : 1', on_pre='ge_post += w')
-    connections['AeAi'].connect(True) # all-to-all connection
-    connections['AeAi'].w = weightMatrix[connections['AeAi'].i, connections['AeAi'].j]
-
-    weightMatrix = get_matrix_from_file(data_path + 'random/AiAe.npy', n_i, n_e)
-    connections['AiAe'] = b.Synapses(neuron_groups['Ai'], neuron_groups['Ae'], model='w : 1', on_pre='gi_post += w')
-    connections['AiAe'].connect(True) # all-to-all connection
-    connections['AiAe'].w = weightMatrix[connections['AiAe'].i, connections['AiAe'].j]
-
-    print( 'create monitors for Ae')
-    spike_counters['Ae'] = b.SpikeMonitor(neuron_groups['Ae'])
-
-    #------------------------------------------------------------------------------
-    # create input population and connections from input populations
-    #------------------------------------------------------------------------------
-    input_groups['Xe'] = b.PoissonGroup(n_input, 0*Hz)
-
-    print( 'create connections between X and A')
-    if test_mode:
-        weightMatrix = get_matrix_from_file(data_path + 'weights/XeAe.npy', n_input, n_e)
-    else:
-        weightMatrix = get_matrix_from_file(data_path + 'random/XeAe.npy', n_input, n_e)
-    model = 'w : 1'
-    pre = 'ge_post += w'
-    post = ''
-
-    if not test_mode:
-        print( 'create STDP for connection XeAe')
-        model += eqs_stdp_ee
-        pre += '; ' + eqs_stdp_pre_ee
-        post = eqs_stdp_post_ee
-
-    connections['XeAe'] = b.Synapses(input_groups['Xe'], neuron_groups['Ae'],
-                                                model=model, on_pre=pre, on_post=post)
-    minDelay = 0*b.ms
-    maxDelay = 10*b.ms
-    deltaDelay = maxDelay - minDelay
-
-    connections['XeAe'].connect(True) # all-to-all connection
-    connections['XeAe'].delay = 'minDelay + rand() * deltaDelay'
-    connections['XeAe'].w = weightMatrix[connections['XeAe'].i, connections['XeAe'].j]
-
-
-    #------------------------------------------------------------------------------
-    # run the simulation and set inputs
-    #------------------------------------------------------------------------------
-
-    net = Network()
-    for obj_list in [neuron_groups, input_groups, connections, spike_counters]:
-        for key in obj_list:
-            net.add(obj_list[key])
-
-    
-    net.run(0*second)
-    input_groups['Xe'].rates = rate * Hz
-    net.run(single_example_time, report='text')
-    input_groups['Xe'].rates = 0 * Hz
-    net.run(resting_time)
-    if j == 0:
-        device.build(directory='output', compile=True, run=False, debug=False)
-    else:
-        device.run(device.project_dir, with_output=False, run_args=[])
-    return spike_counters
-
-
-#------------------------------------------------------------------------------
-# load MNIST
-#------------------------------------------------------------------------------
-training = get_labeled_data(MNIST_data_path + 'training')
-testing = get_labeled_data(MNIST_data_path + 'testing', bTrain = False)
-
 
 #------------------------------------------------------------------------------
 # set parameters and equations
@@ -311,6 +148,165 @@ if test_mode:
         result_monitor = np.zeros((num_examples,n_e))
 
 
+single_example_time =   0.35 * b.second
+resting_time = 0.15 * b.second
+runtime = num_examples * (single_example_time + resting_time)
+
+use_testing_set       = True # add
+
+v_rest_e = -65. * b.mV
+v_rest_i = -60. * b.mV
+v_reset_e = -65. * b.mV
+v_reset_i = 'v=-45.*mV'
+v_thresh_i = 'v>-40.*mV'
+refrac_e = 5. * b.ms
+refrac_i = 2. * b.ms
+
+
+tc_pre_ee = 20*b.ms
+tc_post_1_ee = 20*b.ms
+tc_post_2_ee = 40*b.ms
+nu_ee_pre =  0.0001      # learning rate
+nu_ee_post = 0.01       # learning rate
+wmax_ee = 1.0
+
+if test_mode:
+    scr_e = 'v = v_reset_e; timer = 0*ms'
+else:
+    tc_theta = 1e7 * b.ms
+    theta_plus_e = 0.05 * b.mV
+    scr_e = 'v = v_reset_e; theta += theta_plus_e; timer = 0*ms'
+offset = 20.0*b.mV
+v_thresh_e = '(v>(theta - offset + -52.*mV)) and (timer>refrac_e)'
+
+
+neuron_eqs_e = '''
+        dv/dt = ((v_rest_e - v) + (I_synE+I_synI) / nS) / (100*ms)  : volt (unless refractory)
+        I_synE = ge * nS *         -v                           : amp
+        I_synI = gi * nS * (-100.*mV-v)                          : amp
+        dge/dt = -ge/(1.0*ms)                                   : 1
+        dgi/dt = -gi/(2.0*ms)                                  : 1
+        '''
+if test_mode:
+    neuron_eqs_e += '\n  theta      :volt'
+else:
+    neuron_eqs_e += '\n  dtheta/dt = -theta / (tc_theta)  : volt'
+neuron_eqs_e += '\n  dtimer/dt = 0.1  : second'
+
+neuron_eqs_i = '''
+        dv/dt = ((v_rest_i - v) + (I_synE+I_synI) / nS) / (10*ms)  : volt (unless refractory)
+        I_synE = ge * nS *         -v                           : amp
+        I_synI = gi * nS * (-85.*mV-v)                          : amp
+        dge/dt = -ge/(1.0*ms)                                   : 1
+        dgi/dt = -gi/(2.0*ms)                                  : 1
+        '''
+
+eqs_stdp_ee = '''
+                post2before                            : 1
+                dpre/dt   =   -pre/(tc_pre_ee)         : 1 (event-driven)
+                dpost1/dt  = -post1/(tc_post_1_ee)     : 1 (event-driven)
+                dpost2/dt  = -post2/(tc_post_2_ee)     : 1 (event-driven)
+            '''
+eqs_stdp_pre_ee = 'pre = 1.; w = clip(w - nu_ee_pre * post1, 0, wmax_ee)'
+eqs_stdp_post_ee = 'post2before = post2; w = clip(w + nu_ee_post * pre * post2before, 0, wmax_ee); post1 = 1.; post2 = 1.'
+
+neuron_groups = {}
+input_groups = {}
+connections = {}
+spike_counters = {}
+
+
+neuron_groups['Ae'] = b.NeuronGroup(n_e, neuron_eqs_e, threshold= v_thresh_e, refractory= refrac_e, reset= scr_e, method='euler')
+neuron_groups['Ai'] = b.NeuronGroup(n_i, neuron_eqs_i, threshold= v_thresh_i, refractory= refrac_i, reset= v_reset_i, method='euler')
+
+
+#------------------------------------------------------------------------------
+# create network population and recurrent connections
+#------------------------------------------------------------------------------
+print( 'create neuron group A')
+
+neuron_groups['Ae'].v = v_rest_e - 40. * b.mV
+neuron_groups['Ai'].v = v_rest_i - 40. * b.mV
+if test_mode:
+    neuron_groups['Ae'].theta = np.load(data_path + 'weights/theta_A.npy') * b.volt
+else:
+    neuron_groups['Ae'].theta = np.ones((n_e)) * 20.0*b.mV
+
+print( 'create recurrent connections')
+weightMatrix = get_matrix_from_file(data_path + 'random/AeAi.npy', n_e, n_i)
+connections['AeAi'] = b.Synapses(neuron_groups['Ae'], neuron_groups['Ai'], model='w : 1', on_pre='ge_post += w')
+connections['AeAi'].connect(True) # all-to-all connection
+connections['AeAi'].w = weightMatrix[connections['AeAi'].i, connections['AeAi'].j]
+
+weightMatrix = get_matrix_from_file(data_path + 'random/AiAe.npy', n_i, n_e)
+connections['AiAe'] = b.Synapses(neuron_groups['Ai'], neuron_groups['Ae'], model='w : 1', on_pre='gi_post += w')
+connections['AiAe'].connect(True) # all-to-all connection
+connections['AiAe'].w = weightMatrix[connections['AiAe'].i, connections['AiAe'].j]
+
+print( 'create monitors for Ae')
+spike_counters['Ae'] = b.SpikeMonitor(neuron_groups['Ae'])
+
+#------------------------------------------------------------------------------
+# create input population and connections from input populations
+#------------------------------------------------------------------------------
+input_groups['Xe'] = b.PoissonGroup(n_input, 0*Hz)
+
+print( 'create connections between X and A')
+if test_mode:
+    weightMatrix = get_matrix_from_file(data_path + 'weights/XeAe.npy', n_input, n_e)
+else:
+    weightMatrix = get_matrix_from_file(data_path + 'random/XeAe.npy', n_input, n_e)
+model = 'w : 1'
+pre = 'ge_post += w'
+post = ''
+
+if not test_mode:
+    print( 'create STDP for connection XeAe')
+    model += eqs_stdp_ee
+    pre += '; ' + eqs_stdp_pre_ee
+    post = eqs_stdp_post_ee
+
+connections['XeAe'] = b.Synapses(input_groups['Xe'], neuron_groups['Ae'],
+                                            model=model, on_pre=pre, on_post=post)
+minDelay = 0*b.ms
+maxDelay = 10*b.ms
+deltaDelay = maxDelay - minDelay
+
+connections['XeAe'].connect(True) # all-to-all connection
+connections['XeAe'].delay = 'minDelay + rand() * deltaDelay'
+connections['XeAe'].w = weightMatrix[connections['XeAe'].i, connections['XeAe'].j]
+
+
+param_rate = initialize_parameter(input_groups['Xe'].rates, 0*Hz)
+
+
+#------------------------------------------------------------------------------
+# run the simulation and set inputs
+#------------------------------------------------------------------------------
+
+net = Network()
+for obj_list in [neuron_groups, input_groups, connections, spike_counters]:
+    for key in obj_list:
+        net.add(obj_list[key])
+ 
+
+device.build(directory='sequential', compile=True, run=False, debug=False)
+net.run(0*second)
+
+
+
+
+#------------------------------------------------------------------------------
+# load MNIST
+#------------------------------------------------------------------------------
+training = get_labeled_data(MNIST_data_path + 'training')
+testing = get_labeled_data(MNIST_data_path + 'testing', bTrain = False)
+
+
+
+
+
+
 j = 0
 while j < (int(num_examples)):
     if test_mode:
@@ -318,8 +314,11 @@ while j < (int(num_examples)):
     else:
         normalize_weights()
         rate = training['x'][j%60000,:,:].reshape((n_input)) / 8. *  input_intensity
-    spike_counters = run_network(j, rate, test_mode)
-
+    
+    input_groups['Xe'].rates = rate * Hz
+    net.run(single_example_time, report='text')
+    input_groups['Xe'].rates = 0 * Hz
+    net.run(resting_time)
     current_spike_count = np.asarray(spike_counters['Ae'].count[:]) - previous_spike_count
     previous_spike_count = np.copy(spike_counters['Ae'].count[:])
     if np.sum(current_spike_count) < 5:
